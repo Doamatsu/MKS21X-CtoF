06f52a28fbd504ff55744a1491fe06558f378b2c
Tracy Yang <tyang30@stuy.edu>
2021-09-23 22:32:04 -0400

    Created the file and function outlines.

---
+++ CtoFTester.java
@@ -0,0 +1,12 @@
+public class CtoFTester{
+  public static void celsiusToFahrenheit(){
+
+
+  }
+  public static void farenheitToCelsius(){
+
+  }
+  public static void main(String[] args){
+
+  }
+}
