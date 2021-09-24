43611c443697e92d393da612f937487bb5e6794b
Tracy Yang <tyang30@stuy.edu>
2021-09-23 23:10:26 -0400

    Answered the questions at the top of code.

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,16 @@
+/*
+What should the functions have as parameter(s) ?
+The funtions should have celsius and farenheit
+
+What type should they be?
+They should be doubles since there are fractions/decimals
+
+What should the functions return?
+celsiustoFarenheit should return farenheit value from the celsius value
+and
+farenheitToCelsius should return celsius from the farenheit value
+*/
+
 public class CtoFTester{
   public static void celsiusToFahrenheit(){
 
